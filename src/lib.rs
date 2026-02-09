51888fd81754bd9e21a4ab6394a3e4d511a0e6cf
ininids <ininids@duck.com>
2026-02-09 09:38:06 +0500

    LU

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![allow(clippy::missing_panics_doc)]
 #![allow(clippy::cast_precision_loss)]
 #![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::cast_sign_loss)]
-#![allow(clippy::struct_field_names)]
 pub mod prelude;
 pub mod core;
 pub mod graphics;
 pub mod text;
 pub mod window;
 pub mod audio;
+
+
+// All files should be <200 lines, and all functions should be <20 lines.
